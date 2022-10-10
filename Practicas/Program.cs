48004b135a98e2db1e254b740837921849fda6e3
Alberto <albertomorenogonzalez03@gmail.com>
2022-10-10 12:58:18 +0200

    Agregar archivos de proyecto.

---
+++ Practicas/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hola_mundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca el radio (en cm):");
+
+            int r = int.Parse(Console.ReadLine());
+
+            const double PI = 3.14;
+
+            Console.WriteLine($"El área del círculo de radio {r} cm es {PI * Math.Pow(r, 2)} cm");
+        }
+    }
+}
+
