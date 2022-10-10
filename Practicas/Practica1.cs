f1034c603b19c181797b3846f1dbf78bf264c89b
Alberto <albertomorenogonzalez03@gmail.com>
2022-10-10 13:00:52 +0200

    Agregado Practica1

--- Practicas/Practica1.cs
+++ Practicas/Practica1.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace hola_mundo
+namespace practica1
 {
-    internal class Program
+    internal class Practica1
     {
         static void Main(string[] args)
         {
