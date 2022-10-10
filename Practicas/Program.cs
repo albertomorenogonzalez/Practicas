82c1bf512e06f0de47ff40da0cb6bf18f86c8677
Alberto <albertomorenogonzalez03@gmail.com>
2022-10-10 14:28:31 +0200

    Código ordenado (#region) y añadida práctica 2

--- Practicas/Program.cs
+++ Practicas/Program.cs
@@ -8,7 +8,9 @@ namespace practica1
 {
     internal class Program
     {
-        static void Main(string[] args)
+        //Práctica 1
+        #region
+        static void Practica1()
         {
             Console.WriteLine("Introduzca el radio (en cm):");
 
@@ -18,6 +20,46 @@ namespace practica1
 
             Console.WriteLine($"El área del círculo de radio {r} cm es {PI * Math.Pow(r, 2)} cm");
         }
+        #endregion
+
+        //Práctica 2
+        #region
+        static void Practica2()
+        {
+            const int PUEDE_CONDUCIR = 18;
+            const string TIENE_CARNET = "S";
+
+            Console.WriteLine("Introduce tu edad:");
+            int edad = int.Parse(Console.ReadLine());
+
+            if (edad >= PUEDE_CONDUCIR)
+            {
+                Console.WriteLine("¿Tienes carnet de conducir?");
+                string respuesta = Console.ReadLine().ToUpper();
+
+                if (string.Equals(respuesta, TIENE_CARNET))
+                {
+                    Console.WriteLine("Puedes conducir");
+                }
+                else
+                {
+                    Console.WriteLine("No puedes conducir");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No eres mayor de edad para poder conducir");
+            }
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            //Practica1();
+            Practica2();
+        }
     }
+
+    
 }
 
